7502801a8a3e445426f09bb02ce2992faaa6aaab
TanqDev88 <oliverprim783@gmail.com>
2024-08-05 00:58:22 -0300

    Add commponent home

--- UserLiteraryContributions/src/app/app.module.ts
+++ UserLiteraryContributions/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommentsComponent } from './components/comments/comments.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommentsComponent
+    CommentsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
