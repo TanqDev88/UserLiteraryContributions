25127a507da78ef0156407997426ceb747e035e3
TanqDev88 <oliverprim783@gmail.com>
2024-08-07 02:41:50 -0300

    Modify posts service

--- UserLiteraryContributions/src/app/services/posts.service.ts
+++ UserLiteraryContributions/src/app/services/posts.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../models/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getPostsByUser():Observable<Post[]>{
+    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+  }
 }
