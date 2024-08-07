892e1bfc8652c6f1802cd4aa32c1589f928ce076
TanqDev88 <oliverprim783@gmail.com>
2024-08-07 02:26:28 -0300

    Modify comments service

--- UserLiteraryContributions/src/app/services/comments.service.ts
+++ UserLiteraryContributions/src/app/services/comments.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Comentario } from '../models/comment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getCommentsByPost():Observable<Comentario[]>{
+    return this.http.get<Comentario[]>('https://jsonplaceholder.typicode.com/comments');
+  }
 }
