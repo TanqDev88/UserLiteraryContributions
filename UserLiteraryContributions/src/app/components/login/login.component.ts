09ae3975c849108a1df1ba4146b5c8ea4351f3ef
TanqDev88 <oliverprim783@gmail.com>
2024-08-08 20:18:20 -0300

    Add sevice users

--- UserLiteraryContributions/src/app/components/login/login.component.ts
+++ UserLiteraryContributions/src/app/components/login/login.component.ts
@@ -7,12 +7,11 @@ import { UsersService } from 'src/app/services/users.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  validMessage ='';
- users!: User[];
+  validMessage = '';
+  users!: User[];
 
   loginForm: FormGroup;
 
@@ -20,46 +19,42 @@ export class LoginComponent implements OnInit {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.email, Validators.required]),
       //Minimum eight characters, at least one uppercase letter, one lowercase letter and one number:
-      zipcode: new FormControl('', [Validators.required])
+      zipcode: new FormControl('', [Validators.required]),
     });
-
   }
 
   ngOnInit(): void {
     this.usersService.getUsers().subscribe(
-      data => this.users=data,
-      error => console.error('Fallo el request al API REST')
-
+      (data) => (this.users = data),
+      (error) => console.error('Fallo el request al API REST')
     );
     localStorage.clear();
   }
 
-  get controles(){
+  get controles() {
     return this.loginForm.controls;
   }
 
-  submitLogin(){
+  submitLogin() {
     if (this.loginForm.valid) {
       var email = this.loginForm.value.email;
       var zipcode = this.loginForm.value.zipcode;
-      this.validMessage='';
+      this.validMessage = '';
 
-
-        for (let i = 0; i < this.users.length; i++) {
-
-          if(this.users[i].email== email && this.users[i].address.zipcode == zipcode){
-            localStorage.setItem("Usuario", email);
-            localStorage.setItem("Id", this.users[i].id.toString());
-            this.router.navigate(['/']);
-          }
-
+      for (let i = 0; i < this.users.length; i++) {
+        if (
+          this.users[i].email == email &&
+          this.users[i].address.zipcode == zipcode
+        ) {
+          localStorage.setItem('Usuario', email);
+          localStorage.setItem('Id', this.users[i].id.toString());
+          this.router.navigate(['/']);
         }
+      }
 
-        this.validMessage = "Email o password incorrectos"
-        this.loginForm.reset();
-
+      this.validMessage = 'Email o password incorrectos';
+      this.loginForm.reset();
+      this.ngOnInit;
     }
   }
-
-
 }
