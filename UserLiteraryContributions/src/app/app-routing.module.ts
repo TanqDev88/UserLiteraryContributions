89f534194cdfd41285f2988b9caed9b50cc82b27
TanqDev88 <oliverprim783@gmail.com>
2024-08-08 20:21:45 -0300

    Modify app-routing file

--- UserLiteraryContributions/src/app/app-routing.module.ts
+++ UserLiteraryContributions/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComentariosComponent } from './components/comentarios/comentarios.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path:'comments/:id',
+    component: ComentariosComponent,
+    canActivate:[AuthGuard]
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: "full"
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
